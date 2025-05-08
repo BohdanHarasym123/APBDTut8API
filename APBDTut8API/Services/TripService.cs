c3b9948a8cd2b17ce9a00ad18a1a2165d5adc217
Bohdan Harasym <bohdan@Myroslavas-MacBook-Pro.local>
2025-05-08 11:04:43 +0200

    Added TripService GetTripsAsync() method

--- APBDTut8API/Services/TripService.cs
+++ APBDTut8API/Services/TripService.cs
@@ -10,5 +10,35 @@ public class TripService : ITripService
     {
         _connectionString = "Data Source=localhost, 1433; User=SA; Password=yourStrong()Password; Initial Catalog=apbd; Integrated Security=False;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False";
     }
-    
+
+    public async Task<IEnumerable<Trip>> GetTripsAsync()
+    {
+        var trips = new List<Trip>();
+        
+        using (var connection = new SqlConnection(_connectionString))
+        using (var command = new SqlCommand(
+                   "SELECT * FROM trips",
+                   connection))
+        {
+            await connection.OpenAsync();
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    var id = reader.GetOrdinal("IdTrip");
+                    trips.Add(new Trip
+                    {
+                        IdTrip = reader.GetInt32(id),
+                        Name = reader.GetString(1),
+                        Description = reader.GetString(2),
+                        DateFrom = reader.GetDateTime(3),
+                        DateTo = reader.GetDateTime(4),
+                        MaxPeople = reader.GetInt32(5),
+                    });
+                }
+            }
+        }
+        
+        return trips;
+    }
 }
